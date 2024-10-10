f66833ffa1e97e5de53576e48d85922e1fc3075c
Andrew Snodgrass <pointlander@gmail.com>
2024-10-09 22:01:21 -0500

    Added histogram

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
 )
 
 func main() {
@@ -28,15 +32,32 @@ func main() {
 				max = v
 			}
 		}
+		samples := make(plotter.Values, 0, 8)
 		entropy := 0.0
 		for j := 0; j < outputs.Rows; j++ {
+			rowEntropy := 0.0
 			for k := 0; k < outputs.Cols; k++ {
 				value := real(outputs.Data[j*outputs.Cols+k])
 				if value == 0 {
 					continue
 				}
 				entropy += value * math.Log2(value)
+				rowEntropy += value * math.Log2(value)
 			}
+			samples = append(samples, -rowEntropy)
+		}
+		p := plot.New()
+		p.Title.Text = "entropy"
+
+		histogram, err := plotter.NewHist(samples, 100)
+		if err != nil {
+			panic(err)
+		}
+		p.Add(histogram)
+
+		err = p.Save(8*vg.Inch, 8*vg.Inch, fmt.Sprintf("output/%d_entropy.png", i))
+		if err != nil {
+			panic(err)
 		}
 		entropy = -entropy
 		entropy /= float64(outputs.Rows)
